a38a6f714dbba5460fd6df3fd97f99b3e756e5bb
Vince McKeown <vmckeown79@gmail.com>
2021-10-20 10:14:29 -0400

    WIP on patrolEnemy class

--- js/patrolEnemy.js
+++ js/patrolEnemy.js
@@ -6,6 +6,25 @@ function PatrolEnemy (){
   this.speedY = 0;
   this.runSpeed = 3;
 
+  this.init = function(whichGraphic, whichName) {
+    this.myBitmap = whichGraphic;
+    this.myName = whichName;
+    this.reset();
+  }
+
+  this.reset = function() {
+    for (var i = 0; i < roomGrid.length; i++) {
+        if (roomGrid[i] == TILE_PLAYER) {
+            var tileRow = Math.floor(i / ROOM_COLS);
+            var tileCol = i % ROOM_COLS;
+            this.homeX = tileCol * TILE_W + 0.5 * TILE_W;
+            this.homeY = tileRow * TILE_H + 0.5 * TILE_H;
+            if (worldEditor == false) {
+                roomGrid[i] = TILE_GROUND;
+            }
+            break; // found it, so no need to keep searching 
+        } // end of if
+  } // end of for
 
   this.move = function(){
     if(this.moveLeft){
