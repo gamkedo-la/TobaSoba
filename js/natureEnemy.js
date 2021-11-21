028a1e5a7a8da8c410eab693e41d747ff6b59f50
Adgemon <e.t.khani@gmail.com>
2021-11-21 08:26:36 -0800

    Experimented on nature enemy

--- js/natureEnemy.js
+++ js/natureEnemy.js
@@ -25,8 +25,8 @@ const NATUREENEMY_RADIUS = 25;
     this.move = function(){
       if (dist(this.x - jumperX, this.y - jumperY) < 3*TILE_W) {
         var toPlayer = angTo(jumperX - this.x, jumperY - this.y);
-        this.xv = Math.cos(toPlayer);
-        this.yv = Math.sin(toPlayer);
+        this.xv =  Math.sign(jumperX)*-1 * JUMP_POWER * Math.cos(toPlayer);
+        this.yv = Math.sign(jumperY)*-1 * JUMP_POWER * Math.sin(toPlayer);;
       }
       var nextX = this.x + this.xv;
       var nextY =  this.y + this.yv;
@@ -44,6 +44,34 @@ const NATUREENEMY_RADIUS = 25;
       } else {
         this.x = nextX;
         this.y = nextY;
+      }
+      
+      nextY = this.y + this.speedY;
+      if(this.moveLeft){
+        nextX = this.x + this.speedX - this.width/2;
+      }
+      if(this.moveRight){
+        nextX = this.x + this.speedX + this.width/2;
+      }
+
+      var walkIntoTileIndex = getTileIndexAtPixelCoord(nextX, nextY);
+     // var walkIntoTileType = TILE_WALL;
+  
+      if (walkIntoTileIndex != undefined) {
+          walkIntoTileType = roomGrid[walkIntoTileIndex];
+      }
+  
+      switch (walkIntoTileType) {
+          case TILE_SKY:
+          case TILE_TREASURE:
+          case TILE_SNACK:
+            this.x = nextX;
+            this.y = nextY;
+              break;
+          case TILE_DOOR:
+          case TILE_WALL:
+            case TILE_PLAYER:
+              break;
       }
     }
 
