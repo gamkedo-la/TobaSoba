665f6642df611ce134dc99d2adfc368275a5b9f6
Adgemon <e.t.khani@gmail.com>
2022-01-06 19:55:42 -0800

    Added story text

--- js/ui.js
+++ js/ui.js
@@ -115,4 +115,31 @@ function drawMenu() {
     canvasContext.fillText("Press key P to play", INDENT, 320);
     canvasContext.fillText("Press key M for menu", INDENT, 360);
   }
+
+  function drawPrologue() {
+    colorRect(0, 0, canvas.width/1.5, canvas.height/1.5, 'beige');
+    canvasContext.font = "30px Verdana";
+    canvasContext.fillStyle = 'purple';
+    canvasContext.fillText("I feel lonely and lost", INDENT, 80);
+    canvasContext.fillText("Soba's not been around for a year already.", INDENT, 100);
+    canvasContext.fillText("wait a minure!I hear a murrmuring voice. Maybe I'm going crazy. Whose voice is it?", INDENT, 140);
+    canvasContext.fillText("HHHHe? Hlp? Hlp PLsss? ", INDENT, 180);
+    canvasContext.fillText("Is that you Soba? Sounds like Soba but where could he be?", INDENT, 220);
+    canvasContext.fillText("Oh, I guess it's coming from the Factory!", INDENT, 260);
+    canvasContext.fillText("Maybe I should follow the voice but it's dangerous going there.", INDENT, 300);
+    canvasContext.fillText("I should probably go there and see if Soba's in there", INDENT, 300);
+  }
   
+  function drawEpilogue() {
+    colorRect(0, 0, canvas.width/1.5, canvas.height/1.5, 'beige');
+    canvasContext.font = "30px Verdana";
+    canvasContext.fillStyle = 'purple';
+    canvasContext.fillText("Finally defeated the angry robot! Now where did you hide Soba?", INDENT, 80);
+    canvasContext.fillText("wait, I can see Soba's soul.Why was Soba in there?", INDENT, 100);
+    canvasContext.fillText("Oh no, I destroyed the last remaining of Soba!", INDENT, 140);
+    canvasContext.fillText("Hi Toba ", INDENT, 180);
+    canvasContext.fillText("It's okay. I was imprisoned here", INDENT, 220);
+    canvasContext.fillText("But I could save you", INDENT, 260);
+    canvasContext.fillText("You freed me. That's enough for me.", INDENT, 300);
+    canvasContext.fillText("Thank you Toba.", INDENT, 340);
+  }
